d99c824d455155f66b4eb896296eb8a3158cc7d2
JR Boyens & Roberto Ostinelli <pair+jboyens+rostinelli@pivotallabs.com>
2012-05-03 16:42:59 -0700

    label files are now generated using the tracking number in their name, so that html can load the appropriate gif

--- test/unit/acceptance_response_test.rb
+++ test/unit/acceptance_response_test.rb
@@ -8,29 +8,29 @@ class AcceptanceResponseTest < Test::Unit::TestCase
   end
 
   def test_save_image_file
-    file_path_basename_1 = '/tmp/test_save_image_file_1'
-    file_path_basename_2 = '/tmp/test_save_image_file_2'
+    file_path_1 = '/tmp'
+    file_path_2 = '/tmp'
 
-    @acceptance_response.save_files_for_package_with_tracking_number '1Z9196170297067329', file_path_basename_1
-    @acceptance_response.save_files_for_package_with_tracking_number '1Z9196170295190136', file_path_basename_2
+    @acceptance_response.save_files_for_package_with_tracking_number '1Z9196170297067329', file_path_1
+    @acceptance_response.save_files_for_package_with_tracking_number '1Z9196170295190136', file_path_2
 
-    file_contents_1_gif = open("#{file_path_basename_1}.gif", 'rb') { |io| io.read }
-    file_contents_1_html = open("#{file_path_basename_1}.html", 'rb') { |io| io.read }
-    file_contents_2_gif = open("#{file_path_basename_2}.gif", 'rb') { |io| io.read }
-    file_contents_2_html = open("#{file_path_basename_2}.html", 'rb') { |io| io.read }
+    file_contents_1_gif = open("#{file_path_1}/label1Z9196170297067329.gif", 'rb') { |io| io.read }
+    file_contents_1_html = open("#{file_path_1}/label1Z9196170297067329.html", 'rb') { |io| io.read }
+    file_contents_2_gif = open("#{file_path_2}/label1Z9196170295190136.gif", 'rb') { |io| io.read }
+    file_contents_2_html = open("#{file_path_2}/label1Z9196170295190136.html", 'rb') { |io| io.read }
 
     assert_equal file_contents_1_gif, @acceptance_response.packages.first[:image_data]
     assert_equal file_contents_1_html, @acceptance_response.packages.first[:label_html]
     assert_equal file_contents_2_gif, @acceptance_response.packages.second[:image_data]
     assert_equal file_contents_2_html, @acceptance_response.packages.second[:label_html]
 
-    File.delete "#{file_path_basename_1}.gif"
-    File.delete "#{file_path_basename_1}.html"
-    File.delete "#{file_path_basename_2}.gif"
-    File.delete "#{file_path_basename_2}.html"
+    File.delete "#{file_path_1}/label1Z9196170297067329.gif"
+    File.delete "#{file_path_1}/label1Z9196170297067329.html"
+    File.delete "#{file_path_2}/label1Z9196170295190136.gif"
+    File.delete "#{file_path_2}/label1Z9196170295190136.html"
 
     error = assert_raise RuntimeError do
-      @acceptance_response.save_files_for_package_with_tracking_number 'INVALID', file_path_basename_1
+      @acceptance_response.save_files_for_package_with_tracking_number 'INVALID', file_path_1
     end
     assert_equal 'Invalid Tracking Number', error.message
   end
